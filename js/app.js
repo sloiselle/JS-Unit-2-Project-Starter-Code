1afc9c4986aa18d1185755f8732ac50637b6af14
Scott Loiselle <sloiselle@draftkings.com>
2016-05-17 19:51:36 -0400

    Adding notes

--- js/app.js
+++ js/app.js
@@ -4,4 +4,26 @@
 
 // NPR API Key: kIBQCCCffeYXhxGTvLmokAqDXNYS54wU2ngMpCWv
 
-//
+//Use handlebars.js to template articles
+//Make AJAX request to NPR
+//Add second source later
+//Create a 'loading' container
+	//While articles.length === 0, show loader - something like that
+//Clicking on article should pop overlay/modal with full article
+	//Needs onclick event
+//Should be link in overlay to read more from source
+	//Onclick, overlay.show. Overlay is built with handlebars
+	//Should be able to close with X button
+//User can select news source which repopulates the page
+//Error handling if app cannot load from selected feed
+	//On error from AJAX request
+//Clicking on Search icon expands search bar
+	//Width = 0 by default. Width = 20%. Add sweet transition here, Scott.
+//Clicking the feedr logo displays default feed (all news sources)
+	//Add onclick event here
+//Searching in the bar by article title
+	//On keyup, query the loaded articles
+//Add infinite scrolling
+	//Load 20 articles by default, load 20 more when bottom of page is hit
+
+
