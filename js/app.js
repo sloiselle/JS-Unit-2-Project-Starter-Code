53bef10d6be30a756f4e7b1fc58bd7c353489ee1
Scott Loiselle <sloiselle@draftkings.com>
2016-05-17 20:16:50 -0400

    Adding task assignments

--- js/app.js
+++ js/app.js
@@ -5,29 +5,26 @@
 // NPR API Key: kIBQCCCffeYXhxGTvLmokAqDXNYS54wU2ngMpCWv
 // Guardian API Key: 695fa638-6cf0-4f26-abb9-21ad316b0b1f
 
-//Use handlebars.js to template articles
-//Make AJAX request to NPR
-//Add second source later
-
-//
-
-//Create a 'loading' container
+//JB - Use handlebars.js to template articles
+//Scott - Make AJAX request to NPR
+//Julie - Add second source later
+//Ray - Create a 'loading' container
 	//While articles.length === 0, show loader - something like that
-//Clicking on article should pop overlay/modal with full article
+//Julie - Clicking on article should pop overlay/modal with full article
 	//Needs onclick event
-//Should be link in overlay to read more from source
+//Julie - Should be link in overlay to read more from source
 	//Onclick, overlay.show. Overlay is built with handlebars
 	//Should be able to close with X button
-//User can select news source which repopulates the page
-//Error handling if app cannot load from selected feed
+//JB - User can select news source which repopulates the page
+//Scott - Error handling if app cannot load from selected feed
 	//On error from AJAX request
-//Clicking on Search icon expands search bar
+//Scott - Clicking on Search icon expands search bar
 	//Width = 0 by default. Width = 20%. Add sweet transition here, Scott.
-//Clicking the feedr logo displays default feed (all news sources)
+//Julie - Clicking the feedr logo displays default feed (all news sources)
 	//Add onclick event here
-//Searching in the bar by article title
+//Jim - Searching in the bar by article title
 	//On keyup, query the loaded articles
-//Add infinite scrolling
+//Jim - Add infinite scrolling
 	//Load 20 articles by default, load 20 more when bottom of page is hit
 
 
