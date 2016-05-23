0c5dd07e3bd9fa1e8b67a78dafca1c5411a371b1
julie1013 <julie1380@verizon.net>
2016-05-22 20:00:04 -0400

    merge

--- js/julie.js
+++ js/julie.js
@@ -1,30 +1,33 @@
 
+
+//   var apiString = "http://content.guardianapis.com/search?api-key=695fa638-6cf0-4f26-abb9-21ad316b0b1f";
+
+//   $.ready(isRedirectedURI())
+
+//     function isRedirectedURI() {
+//           debugger;
+//     $.ajax({
+//           url: apiString,
+//           method: "GET",
+//           dataType: "json",
+//           success: function(data){
+//             handleResponseSuccess(data)
+//           }
+//         })
+//       }
+
+//     function handleResponseSuccess(data){
+//       console.log(data);
+//       var title = data.response.results.webTitle;
+//       var date = data.response.results.webPublicationDate;
+//     }
+
+
+
 $(function(){
-
-  var apiString = "http://content.guardianapis.com/search?api-key=695fa638-6cf0-4f26-abb9-21ad316b0b1f";
-
-  $.ready(isRedirectedURI())
-
-    function isRedirectedURI() {
-          debugger;
-    $.ajax({
-          url: apiString,
-          method: "GET",
-          dataType: "json",
-          success: function(data){
-            handleResponseSuccess(data)
-          }
-        })
-      }
-
-    function handleResponseSuccess(data){
-      console.log(data);
-      var title = data.response.results.webTitle;
-      var date = data.response.results.webPublicationDate;
-    }
-
+  $("ul li").click(function () {
+    alert("hi");
+          $("ul li #").dialog("ul li");
+          return false;
+      });
 })
-
-
-
-
